8b4e1346cd75bf141b13db5214f766332c5366b5
Jean Pinzon <jean.pinzon1@gmail.com>
2022-06-24 15:57:51 -0300

    first commit

---
+++ templates/terraform/6-eks.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "<% .Name %>" {
+  name = "eks-cluster-<% .Name %>"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "<% .Name %>-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.<% .Name %>.name
+}
+
+resource "aws_eks_cluster" "<% .Name %>" {
+  name     = "<% .Name %>"
+  role_arn = aws_iam_role.<% .Name %>.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.<% .Name %>-AmazonEKSClusterPolicy]
+}

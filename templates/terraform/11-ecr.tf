cd7c4c330a297cc7226461df668020230d41266f
Jean Pinzon <jean.pinzon1@gmail.com>
2022-06-27 20:47:33 -0300

    fix: ECR name

--- templates/terraform/11-ecr.tf
+++ templates/terraform/11-ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "<% .Name %>-ecr"
+  name                 = "<% .Name %>"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
